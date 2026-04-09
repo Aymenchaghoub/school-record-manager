7a93101a0157686df1bb95a12c434216a2a65ebb
Chaghoub Aymen <ensm.chaghoub.aymen@gmail.com>
2026-04-09 17:49:06 +0200

    enable api routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,10 +8,13 @@ use App\Http\Middleware\RoleMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
+
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
