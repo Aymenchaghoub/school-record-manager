e7102432c3bf2cf420c47ac34e757bebc9f9e3c8
Chaghoub Aymen <ensm.chaghoub.aymen@gmail.com>
2026-04-16 22:23:46 +0200

    Add more unit tests for grade model

--- tests/Unit/Models/GradeModelTest.php
+++ tests/Unit/Models/GradeModelTest.php
@@ -26,4 +26,24 @@ class GradeModelTest extends TestCase
 
         $this->assertSame('C-', $grade->getLetterGrade());
     }
+
+    public function test_percentage_returns_zero_when_max_value_is_zero(): void
+    {
+        $grade = Grade::factory()->create([
+            'value' => 10,
+            'max_value' => 0,
+        ]);
+
+        $this->assertSame(0.0, (float) $grade->getPercentage());
+    }
+
+    public function test_grade_color_is_green_for_high_scores(): void
+    {
+        $grade = Grade::factory()->create([
+            'value' => 18,
+            'max_value' => 20,
+        ]);
+
+        $this->assertSame('text-green-600', $grade->getGradeColor());
+    }
 }
