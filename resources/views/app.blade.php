5757631c7581ae2052a034c1156a55d9fc97dccc
Chaghoub Aymen <ensm.chaghoub.aymen@gmail.com>
2026-04-16 18:41:02 +0200

    Create SPA shell Blade view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>School Record Manager</title>
+    @viteReactRefresh
+    @vite(['frontend/src/main.jsx'])
 </head>
 <body>
     <div id="root"></div>
